d45f639a8a5b0dea808c12dd679aa02b54f57d27
hazem94 <hazem.alabiad@hotmail.com>
2021-03-15 00:08:26 +0300

    Build tiny containers

--- debian-node-simple-app/index.js
+++ debian-node-simple-app/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 http
   .createServer(function (request, response) {
     console.log("request received");
-    response.end("omg hi", "utf-8");
+    response.end("Hello World!", "utf-8");
   })
   .listen(3000);
 console.log("server started");
